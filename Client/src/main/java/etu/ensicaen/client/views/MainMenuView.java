d79c6c6943553e3210d8249c1cf319eedff20ec1
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2025-06-15 19:10:43 +0200

    second client can join

--- Client/src/main/java/etu/ensicaen/client/views/MainMenuView.java
+++ Client/src/main/java/etu/ensicaen/client/views/MainMenuView.java
@@ -1,16 +1,19 @@
 package etu.ensicaen.client.views;
 
-import etu.ensicaen.client.Client;
 import etu.ensicaen.client.ClientApplication;
 import etu.ensicaen.client.viewsmodels.MainMenuViewModel;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class MainMenuView {
     private MainMenuViewModel viewModel;
+
+    @FXML
+    public TextField sessionIdTextField;
 
     public void init(MainMenuViewModel vm) {
         this.viewModel = vm;
@@ -36,7 +39,21 @@ public class MainMenuView {
     }
 
     public void onJoinAction(ActionEvent actionEvent) throws IOException {
-        /*String response = ClientApplication.client.sendMessage("JOIN:");
-        System.out.println(response);*/
+        Task<String> task = new Task<>() {
+            @Override
+            protected String call() throws Exception {
+                return ClientApplication.client.sendMessage("JOIN:" + sessionIdTextField.getText().trim());
+            }
+        };
+
+        task.setOnSucceeded(ev ->
+                System.out.println(task.getValue())
+        );
+
+        task.setOnFailed(ev ->
+                System.out.println(task.getValue())
+        );
+
+        new Thread(task).start();
     }
 }
