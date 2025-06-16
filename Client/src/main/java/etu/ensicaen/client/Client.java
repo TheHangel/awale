42db280d7463077619a327c9e3d9124997a0e97c
TheHangel <105165180+TheHangel@users.noreply.github.com>
2025-06-16 18:56:15 +0200

    AWL-004 feat: send object

--- Client/src/main/java/etu/ensicaen/client/Client.java
+++ Client/src/main/java/etu/ensicaen/client/Client.java
@@ -1,37 +1,39 @@
 package etu.ensicaen.client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
-    private Socket socket;
-    private BufferedReader in;
-    private PrintWriter out;
+    private Socket             socket;
+    private ObjectInputStream  in;
+    private ObjectOutputStream out;
 
     public Client(String host, int port) throws IOException {
         socket = new Socket(host, port);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new PrintWriter(socket.getOutputStream(), true);
+        out    = new ObjectOutputStream(socket.getOutputStream());
+        in     = new ObjectInputStream (socket.getInputStream());
     }
 
-    public String sendMessage(String message) throws IOException {
-        out.println(message);
-        return in.readLine();
+    public String sendCommand(String cmd) throws IOException, ClassNotFoundException {
+        out.writeObject(cmd);
+        Object resp = in.readObject();
+        return (resp instanceof String) ? (String) resp : null;
     }
 
-    public String host() throws IOException {
-        return this.sendMessage("HOST");
+    public String host() throws IOException, ClassNotFoundException {
+        return this.sendCommand("HOST");
     }
 
-    public String join(String id) throws IOException {
-        return this.sendMessage("JOIN:" + id);
+    public String join(String id) throws IOException, ClassNotFoundException {
+        return this.sendCommand("JOIN:" + id);
     }
 
-    public String readMessage() throws IOException {
-        return in.readLine();
+    public void sendObject(Object obj) throws IOException {
+        out.writeObject(obj);
+    }
+
+    public Object readObject() throws IOException, ClassNotFoundException {
+        return in.readObject();
     }
 
     public void close() throws IOException {
