edbf1603ea32243b834852c11c6e5f91f5e38c10
Celia Dennetiere <celia.dennetiere@ecole.ensicaen.fr>
2025-06-17 16:07:44 +0200

    AWL-003 feat : Session start game (not tested)

--- Server/src/main/java/etu/ensicaen/server/Session.java
+++ Server/src/main/java/etu/ensicaen/server/Session.java
@@ -1,8 +1,7 @@
 package etu.ensicaen.server;
 
+import etu.ensicaen.shared.models.Game;
 import etu.ensicaen.shared.models.Player;
-import etu.ensicaen.shared.models.PlayerScore;
-
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.UUID;
@@ -11,19 +10,16 @@ public class Session {
     private final String id;
     private final Socket hostSocket;
     private       Socket guestSocket;
-
-    private ArrayList<Player>      players;
-    private ArrayList<PlayerScore> scores;
+    private final Player[] players = new Player[2];
+    private ArrayList<Game> games;
 
     public Session(Socket hostSocket) {
         this.id = UUID.randomUUID().toString().substring(0, 8);
-        this.players = new ArrayList<>();
-        this.scores  = new ArrayList<>();
+        this.games = new ArrayList<>();
 
         Player hostPlayer = new Player("name player 1"); // @todo send custom username
         // add the host (who created the session) to the session
-        this.players.add(hostPlayer);
-        this.scores.add(new PlayerScore(hostPlayer));
+        this.players[0] = hostPlayer;
         this.hostSocket = hostSocket;
     }
 
@@ -34,9 +30,8 @@ public class Session {
     public synchronized boolean addGuest(Socket socket) {
         if (guestSocket == null) {
             guestSocket = socket;
-            Player hostPlayer = new Player("name player 2"); // @todo send custom username
-            this.players.add(hostPlayer);
-            this.scores.add(new PlayerScore(hostPlayer));
+            Player guestPlayer = new Player("name player 2"); // @todo send custom username
+            this.players[1]= guestPlayer;
             return true;
         }
         return false;
@@ -51,4 +46,26 @@ public class Session {
         if (s == guestSocket) return hostSocket;
         return null;
     }
+
+    public boolean startGame() { //TODO : test with mock GameBoard
+        Game newGame = new Game(players[0], players[1]);
+        this.games.add(newGame);
+        Player currentPlayer = Math.random() < 0.5 ? players[0] : players[1];
+        int gameFinished = 0;
+        while(gameFinished == 0) {
+            gameFinished = newGame.playTurn(currentPlayer);
+            newGame.getGameBoard();
+            //TODO send gameBoard to clients for them to display it
+            //switch player
+            currentPlayer = (currentPlayer == players[0]) ? players[1] : players[0];
+        }
+        if (gameFinished == -1) {
+            //TODO send draw message to both players
+        } else if (gameFinished == 1) {
+            //TODO send victory message to currentPlayer
+            //TODO send defeat message to the other player
+        }
+        return true;
+    }
+
 }
