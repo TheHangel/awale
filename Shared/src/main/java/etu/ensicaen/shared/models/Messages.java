77abab2c9e5f6959a05d91d0f41a83f1be3dac25
Thibaud Lebrasseur <thibaud.lebrasseur@ecole.ensicaen.fr>
2025-06-22 01:20:55 +0200

    AWL-017 fix : message + fix

--- Shared/src/main/java/etu/ensicaen/shared/models/Messages.java
+++ Shared/src/main/java/etu/ensicaen/shared/models/Messages.java
@@ -7,4 +7,5 @@ public class Messages {
     public static final String ILLEGAL_MESSAGE = "You cannot make illegal move / Vous ne pouvez pas jouer un coup interdit";
     public static final String ILLEGAL_MOVE    = "Illegal move / Mouvement interdit";
     public static final String OVER_MESSAGE    = "Game over / Partie terminé";
+    public static final String LEAVE_MESSAGE   = "Your opponent leaved / Votre adveersaire est parti";
 }
