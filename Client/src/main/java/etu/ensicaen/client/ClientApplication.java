994fcfe09826132e0716838e57259a367a1a9bae
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2025-06-22 06:22:05 +0200

    MAIN fix : fix show message when no server open
    Signed-off-by: Hangel <thibaud.lebrasseur@etu.unicaen.fr>

--- Client/src/main/java/etu/ensicaen/client/ClientApplication.java
+++ Client/src/main/java/etu/ensicaen/client/ClientApplication.java
@@ -4,6 +4,7 @@ import etu.ensicaen.client.handlers.ViewHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -13,7 +14,17 @@ import java.io.IOException;
 public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Client.get(Config.host(), Config.port());
+        try {
+            Client.get(Config.host(), Config.port());
+        }
+        catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Connection Error / Erreur de connexion");
+            alert.setHeaderText("Could not connect to the server / Impossible de se connecter au serveur");
+            alert.setContentText("Please check your connection settings and try again. / Vérifiez vos paramètres de connexion et réessayez.");
+            alert.showAndWait();
+            return;
+        }
 
         Font.loadFont(getClass().getResourceAsStream("etu/ensicaen/client/assets/Game Bubble.ttf"), 10);
         FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("main-menu-view.fxml"));
