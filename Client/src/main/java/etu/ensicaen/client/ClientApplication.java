60f0917c24c7d5439040b349c65e47f2d3994810
Celia Dennetiere <celia.dennetiere@ecole.ensicaen.fr>
2025-06-22 06:12:46 +0200

    AWL-005 style : game style

--- Client/src/main/java/etu/ensicaen/client/ClientApplication.java
+++ Client/src/main/java/etu/ensicaen/client/ClientApplication.java
@@ -2,6 +2,10 @@ package etu.ensicaen.client;
 
 import etu.ensicaen.client.handlers.ViewHandler;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +14,14 @@ public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Client.get(Config.host(), Config.port());
+
+        Font.loadFont(getClass().getResourceAsStream("etu/ensicaen/client/assets/Game Bubble.ttf"), 10);
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("main-menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 350, 450);
+
+        stage.getIcons().add(new Image(this.getClass().getResourceAsStream("assets/logo.png")));
+        stage.setResizable(false);
+        stage.setTitle("Awale Client");
         stage.setOnCloseRequest(ev -> {
             try {
                 Client.get().close();
